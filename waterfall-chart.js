825f750f2232549fadffe1e590548be84ec0a1c6
bradleyaidanjohnson <bradleyaidanjohnson@gmail.com>
2024-08-25 07:13:48 +0100

    cat waterfall working, both need refinement and sub zero axis logic

--- waterfall-chart.js
+++ waterfall-chart.js
@@ -51,7 +51,8 @@ function WaterfallChart() {
   this.preload = function () {
     var self = this;
     this.data = loadTable(
-      "./data/new-data/waterfall.csv",
+      // "./data/new-data/waterfall.csv",
+      "./data/new-data/waterfall-cat.csv",
       "csv",
       "header",
       // Callback function to set the value
@@ -73,15 +74,32 @@ function WaterfallChart() {
     this.yAxisLabel = this.data.columns[1];
 
     var maxHeight = 0;
+    var colCount = this.data.getColumnCount();
+    var rowCount = this.data.getRowCount();
+    // console.log(colCount);
 
-    var colValues = this.data.getColumn(this.data.columns[1]);
-    var colNums = colValues.map(Number);
+    if (colCount === 2) {
+      var colValues = this.data.getColumn(this.data.columns[1]);
+      var colNums = colValues.map(Number);
 
-    var maxHeight = colNums.reduce((accumulator, currentValue) => {
-      return accumulator + currentValue;
-    }, 0);
+      maxHeight = colNums.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      }, 0);
+    } else if (colCount > 2) {
+      for (var i = 0; i < rowCount; i++) {
+        var sum = 0;
+        for (var j = 1; j < colCount; j++) {
+          if (this.data.getNum(i, j) > 0) {
+            sum = sum + this.data.getNum(i, j);
+          }
+        }
+        if (sum > maxHeight) {
+          maxHeight = sum;
+        }
+      }
+    }
 
-    console.log(colNums);
+    console.log(maxHeight);
 
     // Draw the title above the plot.
     this.drawTitle();
@@ -103,52 +121,144 @@ function WaterfallChart() {
 
     // Draw Female/Male labels at the top of the plot.
     this.drawCategoryLabels();
+    if (colCount === 2) {
+      var lineWidth =
+        (this.layout.rightMargin - this.layout.leftMargin) /
+        this.data.getRowCount();
 
-    var lineWidth =
-      (this.layout.rightMargin - this.layout.leftMargin) /
-      this.data.getRowCount();
+      // Loop over every row in the data.
+      var columnTop = this.layout.bottomMargin;
+      for (var i = 0; i < this.data.getRowCount(); i++) {
+        // Calculate the x position for each company.
+        var lineX = lineWidth * i + this.layout.leftMargin;
 
-    // Loop over every row in the data.
-    var columnTop = this.layout.bottomMargin;
-    for (var i = 0; i < this.data.getRowCount(); i++) {
-      // Calculate the x position for each company.
-      var lineX = lineWidth * i + this.layout.leftMargin;
+        // Create an object that stores data from the current row.
+        var columnValue = {
+          // Add each row's data to the columnValue variable
+          name: this.data.getString(i, 0),
+          value: this.data.getNum(i, 1),
+        };
+        // Draw the columnValue name on the bottom margin.
+        fill(0);
+        noStroke();
+        textAlign("center", "bottom");
+        text(
+          columnValue.name,
+          lineX + lineWidth * 0.5,
+          this.layout.bottomMargin + 20
+        );
+        columnTop =
+          (1 - columnValue.value / maxHeight) *
+            (this.layout.bottomMargin - this.layout.topMargin) +
+          this.layout.topMargin -
+          this.layout.bottomMargin +
+          columnTop;
+        // Draw rectangle
+        if (columnValue.value < 0) {
+          fill(255, 0, 0);
+        } else {
+          fill(colorTheme[i % colorTheme.length]);
+        }
 
-      // Create an object that stores data from the current row.
-      var columnValue = {
-        // Add each row's data to the columnValue variable
-        name: this.data.getString(i, 0),
-        value: this.data.getNum(i, 1),
-      };
-      // Draw the columnValue name on the bottom margin.
-      fill(0);
-      noStroke();
-      textAlign("center", "bottom");
-      text(
-        columnValue.name,
-        lineX + lineWidth * 0.5,
-        this.layout.bottomMargin + 20
-      );
-      columnTop =
-        (1 - columnValue.value / maxHeight) *
-          (this.layout.bottomMargin - this.layout.topMargin) +
-        this.layout.topMargin -
-        this.layout.bottomMargin +
-        columnTop;
-      // Draw rectangle
-      if (columnValue.value < 0) {
-        fill(255, 0, 0);
-      } else {
-        fill(colorTheme[i % colorTheme.length]);
+        rect(
+          lineX,
+          columnTop,
+          lineWidth,
+          (this.layout.bottomMargin - this.layout.topMargin) *
+            (columnValue.value / maxHeight)
+        );
       }
+    } else if (colCount > 2) {
+      var lineWidth =
+        (this.layout.rightMargin - this.layout.leftMargin) /
+        (this.data.getRowCount() * this.data.getRowCount());
 
-      rect(
-        lineX,
-        columnTop,
-        lineWidth,
-        (this.layout.bottomMargin - this.layout.topMargin) *
-          (columnValue.value / maxHeight)
-      );
+      for (var i = 0; i < this.data.getRowCount(); i++) {
+        // Loop over every row in the data.
+        var rowSum = 0;
+        var columnTop = this.layout.bottomMargin;
+        for (var j = 1; j < this.data.getColumnCount(); j++) {
+          // i = row, j = column
+          // Calculate the x position for each company.
+          var lineX =
+            lineWidth * ((i + 1) * colCount - (colCount - (j - 1))) +
+            this.layout.leftMargin;
+
+          // Create an object that stores data from the current row.
+          var columnValue = {
+            // Add each row's data to the columnValue variable
+            name: this.data.columns[j],
+            value: this.data.getNum(i, j),
+          };
+          rowSum = rowSum + columnValue.value;
+          // console.log(columnValue.name);
+
+          // Draw the columnValue name on the bottom margin.
+          fill(0);
+          noStroke();
+          textAlign("center", "bottom");
+          text(
+            columnValue.name,
+            lineX + lineWidth * 0.5,
+            this.layout.bottomMargin + 20
+          );
+          columnTop =
+            (1 - columnValue.value / maxHeight) *
+              (this.layout.bottomMargin - this.layout.topMargin) +
+            this.layout.topMargin -
+            this.layout.bottomMargin +
+            columnTop;
+          // Draw rectangle
+          if (columnValue.value < 0) {
+            fill(255, 0, 0);
+          } else {
+            fill(colorTheme[(j + 4) % colorTheme.length]);
+          }
+
+          rect(
+            lineX,
+            columnTop,
+            lineWidth,
+            (this.layout.bottomMargin - this.layout.topMargin) *
+              (columnValue.value / maxHeight)
+          );
+        }
+        // Draw a total rect
+        var lineX =
+          lineWidth * ((i + 1) * colCount - (colCount - (j - 1))) +
+          this.layout.leftMargin;
+
+        // Create an object that stores data from the current row.
+        var columnValue = {
+          // Add each row's data to the columnValue variable
+          name: this.data.getString(i, 0) + " Total",
+          value: rowSum,
+        };
+        fill(0);
+        noStroke();
+        textAlign("center", "bottom");
+        text(
+          columnValue.name,
+          lineX + lineWidth * 0.5,
+          this.layout.bottomMargin + 20
+        );
+        // Draw rectangle
+        if (columnValue.value < 0) {
+          fill(255, 0, 0);
+        } else {
+          fill(0, 0, 0);
+        }
+
+        rect(
+          lineX,
+          (1 - columnValue.value / maxHeight) *
+            (this.layout.bottomMargin - this.layout.topMargin) +
+            this.layout.topMargin,
+          lineWidth,
+          (this.layout.bottomMargin - this.layout.topMargin) *
+            (columnValue.value / maxHeight)
+        );
+      }
     }
   };
 
